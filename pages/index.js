0838f5d068c64fae5f6f0f447765b9afaf68b046
alexisspa9 <alexisspa9@gmail.com>
2022-03-31 15:37:33 +0300

    fixed

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
             <div className={styles.row}>
               <div className={[styles.col_50, styles.pp].join(" ")}>
                 <h1 className={styles.aboutTitle}>Mantalena Bafiti</h1>
-                <p className={styles.aboutText}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                <p className={styles.aboutText}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                             </p>
                 </div>
                 <div className={styles.col_50}>
